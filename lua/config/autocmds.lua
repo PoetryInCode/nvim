92787383ffd6025555b5bf0303003f6e55319838
Solomon Wagner <solow@solow.xyz>
2024-03-27 14:32:27 -0400

    Autocmds for language specific indenting + udpates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.cmd([[
+    autocmd FileType python setlocal shiftwidth=4 tabstop=4
+    autocmd FileType c setlocal shiftwidth=2 tabstop=2
+    autocmd FileType html setlocal shiftwidth=2 tabstop=2
+    autocmd FileType java setlocal shiftwidth=4 tabstop=4
+]])
